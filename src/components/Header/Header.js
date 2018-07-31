9237adb10b064bc862ea5dc0ed7e02a9ce61c20f
unknown <wei57960@163.com>
2018-07-31 18:38:33 +0800

    7-31

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,7 @@ export default class Header extends React.Component {
                 <div className='ava'><Avatar shape="square" style={{ width: '150px', height: '150px' }} src={item.hurl} /></div>
                 <div className='lft'>
                     <p>三班人</p>
-                    <p>学员编号：{item.number}</p>
+                    <p>学员编号：MID330900002</p>
                     <p>在学课程：{item.learningLesson}</p>
                     <p>入学时间：{item.enterDate}</p>
                 </div>
